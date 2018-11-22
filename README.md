6f856b492f313ef6be277939021f78e83c850837
李银池 <yinchi.li@ucarinc.com>
2018-11-22 14:52:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,223 @@
+
+###########Environment 环境依赖 
+node v0.10.28+
+reids ~
+"body-parser": "^1.18.3",
+"express": "^4.16.4",
+"mongodb": "^3.1.10",
+"multer": "^1.4.1"
+
+###########Deployment steps 部署步骤
+1. npm install  //Installation node operation environment 安装node运行环境
+
+2. npm server.js   //Start 启动
+
+
+###########目录结构描述
+├── README.md                   // help
+├── server.js                   // server
+├── mongoDB_demo.js             // mongdb
+
+
+
+###########V1.0.0 Version update
+
+
+English
+1. New features to create the database
+
+Request method: POST
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/CreateDB/
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB3"
+
+}
+
+
+2. New features to create collections
+
+Request method: POST
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/CreateCollection/
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB",
+
+"Collection_name" : "Express_API_Collection1"
+
+}
+
+
+3. The new function to delete data in a database collection
+
+Request method: DELETE
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/delete
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB",
+
+"Collection_name" : "Express_API_Collection1",
+
+"WhereStr" :
+
+{
+
+"Name" : "netease"
+
+}
+
+}
+
+
+4. The new feature collection
+
+Request method: DELETE
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/drop
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB",
+
+"Collection_name" : "Express_API_Collection5"
+
+}
+
+
+5. New function to insert data to the database in the collection
+
+Request method: PUT
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/insert_data/
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB",
+
+"Collection_name" : "Express_API_Collection1",
+
+"Obj" : {
+
+"Name" : "netease",
+
+"Url" : "www.162.com"
+
+}
+
+}
+
+
+6. New functions query the database data in a collection
+
+Request method: POST
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/find
+
+The request body: {
+
+"Db_url" : "mongo: / / localhost: 27017 /",
+
+Db_name: "Express_API_DB",
+
+"Collection_name" : "Express_API_Collection1"
+
+}
+
+
+7. New functions query the database data in a collection
+
+Request method: GET
+
+Request address: http://127.0.0.1:8004/mongoDB_demo/find
+
+Request parameters:
+
+Db_name database name
+
+Collection_name collection name
+
+###########V1.0.0 版本内容更新
+中文
+1. 新功能   创建数据库
+请求方法：POST
+请求地址：http://127.0.0.1:8004/mongoDB_demo/CreateDB/
+请求body：{
+	"db_url":"mongodb://localhost:27017/",
+	"db_name":"Express_API_DB3"
+}
+
+2. 新功能   创建集合
+请求方法：POST
+请求地址：http://127.0.0.1:8004/mongoDB_demo/CreateCollection/
+请求body：{
+	"db_url":"mongodb://localhost:27017/",
+	"db_name":"Express_API_DB",
+	"collection_name":"Express_API_Collection1"
+}
+
+3. 新功能   删除数据库集合中的数据
+请求方法：DELETE
+请求地址：http://127.0.0.1:8004/mongoDB_demo/delete
+请求body：{
+	"db_url":"mongodb://localhost:27017/",
+	"db_name":"Express_API_DB",
+	"collection_name":"Express_API_Collection1",
+	"whereStr":
+		{
+			"name":"网易"			
+		}
+}
+
+4. 新功能   删除集合
+请求方法：DELETE
+请求地址：http://127.0.0.1:8004/mongoDB_demo/drop
+请求body：{
+	"db_url":"mongodb://localhost:27017/",
+	"db_name":"Express_API_DB",
+	"collection_name":"Express_API_Collection5"
+}
+
+5. 新功能   插入数据到数据库集合中
+请求方法：PUT
+请求地址：http://127.0.0.1:8004/mongoDB_demo/insert_data/
+请求body：{ 
+	"db_url":"mongodb://localhost:27017/",
+	"db_name":"Express_API_DB",
+	"collection_name":"Express_API_Collection1",
+	"obj":{
+		"name": "网易",
+		"url": "www.162.com"
+		}
+}
+
+6. 新功能   查询数据库集合中的数据
+请求方法：POST
+请求地址：http://127.0.0.1:8004/mongoDB_demo/find
+请求body：{
+		"db_url":"mongodb://localhost:27017/",
+		"db_name":"Express_API_DB",
+		"collection_name":"Express_API_Collection1"
+}
+
+7. 新功能   查询数据库集合中的数据
+请求方法：GET
+请求地址：http://127.0.0.1:8004/mongoDB_demo/find
+请求参数：
+db_name数据库名称
+collection_name集合名称
---
+++ mongoDB_demo.js
@@ -0,0 +1,204 @@
+//引入express包
+var express=require("express");
+//实例化一个express对象
+var router=express.Router();
+//引入body-parser解析包
+var bodyParser = require('body-parser');
+//引入mongDB包
+var MongoClient = require('mongodb').MongoClient;
+//告诉路由中间件，使用bodyparser解析json
+router.use(bodyParser.json());
+router.all(function(req,res,next){
+    console.log('Time:', new Date());
+    console.info("请求方法："+req.method+";请求path："+req.path);
+});
+
+/**
+ * 功能：创建数据库
+ * 作者：李银池
+ * 参数：
+ * db_name 创建数据库名称
+ *  */
+router.post("/CreateDB",function(req,res,next){  
+   
+    MongoClient.connect(req.body.db_url, { useNewUrlParser: true } ,function (err, db) {
+    if (err) throw err;
+        console.log('数据库已创建');
+        db.db(req.body.db_name);
+        console.info("数据库名称："+req.body.db_name);
+        //断开数据库连接
+        db.close;
+        });
+        //响应body
+        res.send({"result":"success","status":200,"db_name":req.body.db_name});
+       //释放请求-响应周期
+        res.end();
+       
+});
+
+/**
+ * 功能：创建数据库的集合
+ * 作者：李银池
+ * 参数：
+ * db_name 创建数据库名称
+ * collection_name 创建数据库中集合名称
+ *  */
+router.post("/CreateCollection",function(req,res,next){  
+   
+    MongoClient.connect(req.body.db_url, { useNewUrlParser: true } ,function (err, db) {
+    if (err) throw err;
+    console.info('数据库已创建');
+    var dbase = db.db(req.body.db_name);
+    console.info("数据库名称："+req.body.db_name+"，"+"集合名称："+req.body.collection_name);
+    dbase.createCollection(req.body.collection_name, function (err, res) {
+        if (err) throw err;
+        console.info("创建集合"+req.body.collection_name+"成功");
+        //关闭数据库连接
+        db.close();
+    });
+});    //响应body
+        res.send({"result":"success","status":200,"db_name":req.body.db_name,"collection_name":req.body.collection_name});
+       //释放请求-响应周期
+        res.end();
+});
+
+
+/**
+ * 功能：查询数据库的集合中所有数据
+ * 作者：李银池
+ * 参数：
+ * :db_name 查询的数据库名称
+ * :collection_name 查询的数据库中集合名称
+ *  */
+router.get("/find/db_name/:db_name/collection_name/:collection_name",function(req,res,next){
+    var url = 'mongodb://localhost:27017/';
+    MongoClient.connect(url,{ useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(req.params.db_name);
+        console.info("请求参数的数据库名称为："+req.params.db_name);
+        dbo.collection(req.params.collection_name). find({}).toArray(function(err, result) { // 返回集合中所有数据
+            if (err) throw err;
+            console.info("查询结果"+result);
+            //响应body
+            res.send(JSON.stringify(result));
+             //关闭数据库连接
+            db.close();
+        });
+    });
+
+
+});
+
+/**
+ * 功能：查询数据库的集合中所有数据
+ * 作者：李银池
+ * 参数：
+ * db_url 查询数据库的地址
+ * db_name 查询的数据库名称
+ * Collection_name 查询的数据库中集合名称
+ *  */
+router.post("/find",function(req,res,next){
+    MongoClient.connect(req.body.db_url,{ useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(req.body.db_name);
+         dbo.collection(req.body.collection_name).find({}).toArray(function(err, result) { // 返回集合中所有数据
+            if (err) throw err;
+            console.info("查询结果："+result);
+            //响应body
+            res.send(JSON.stringify(result));
+             //关闭数据库连接
+            db.close(); 
+        });
+    });
+    console.info("请求参数的数据库地址："+req.body.db_url);
+    console.info("请求参数的数据库名称为："+req.body.db_name);
+    console.info("请求参数的数据库集合称为："+req.body.collection_name);
+    
+});
+
+/**
+ * 功能：数据库集合插入数据
+ * 作者：李银池
+ * 参数：
+ * db_url 查询数据库的地址
+ * db_name 查询的数据库名称
+ * Collection_name 查询的数据库中集合名称
+ * obj 插入集合的数据
+ *  */
+router.put("/insert_data",function(req,res,next){
+    MongoClient.connect(req.body.db_url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(req.body.db_name);
+        dbo.collection(req.body.collection_name).insertOne(req.body.obj, function(err, res) {
+            if (err) throw err;
+            console.log("文档插入成功");
+            //关闭数据库连接
+            db.close();
+        });
+    });
+    res.send({"result":"success","status":200,"插入数据":req.body.obj});
+    //释放请求-响应周期
+    res.end();
+});
+
+/**
+ * 功能：数据库集合插入数据
+ * 作者：李银池
+ * 参数：
+ * db_url 查询数据库的地址
+ * db_name 查询的数据库名称
+ * Collection_name 查询的数据库中集合名称
+ * whereStr 删除符合条件的记录
+ *  */
+router.delete("/delete",function(req,res,next){
+    MongoClient.connect(req.body.db_url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(req.body.db_name);
+        var whereStr = req.body.whereStr; 
+        dbo.collection(req.body.collection_name).deleteOne(whereStr, function(err, obj) {
+            if (err) throw err;
+            console.log("文档删除成功");
+            //响应body
+            res.send({"result":"success","status":200,"删除数据":req.body.obj});
+            //关闭数据库连接
+            db.close();
+        });
+    });
+
+});
+
+/**
+ * 功能：数据库集合插入数据
+ * 作者：
+ * 参数：
+ * db_url 查询数据库的地址
+ * db_name 查询的数据库名称
+ * Collection_name 查询的数据库中集合名称
+ * whereStr 删除符合条件的记录
+ *  */
+router.delete("/drop",function(req,res,next){
+    MongoClient.connect(req.body.db_url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(req.body.db_name);
+
+       // 删除 test 集合
+    dbo.collection(req.body.collection_name).drop(function(err, delOK) { 
+         // 执行成功 delOK 返回 true，否则返回 false
+            if (err) throw err;
+            console.log("文档删除成功");
+            if(delOK===true){
+                //响应body
+                res.send({"result":"success","status":200,"删除集合成功":req.body.collection_name});
+            }else{
+                //响应body
+                res.send({"result":"failed","status":300,"删除集合失败":req.body.collection_name});
+            }
+            
+            //关闭数据库连接
+            db.close();
+        });
+    });
+
+});
+
+module.exports=router;
